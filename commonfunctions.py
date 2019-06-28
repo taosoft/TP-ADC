8d55281d862ce71f1e25917bc7757d1ee456c39a
MATIAS-ASUS <matigrieben@est.frba.utn.edu.ar>
2019-06-28 12:58:28 -0300

    Funcionando (faltan detalles)

--- commonfunctions.py
+++ commonfunctions.py
@@ -1,4 +1,7 @@
 import time
+import numpy as np
+import struct
+import hashlib
 
 # Clase ticker
 #-------------------------------------------
@@ -33,3 +36,80 @@ def printHashBinary(digesto, spacer='', breakOn=4):
     print('-------- BEGIN HASH BINARY --------')
     print(hstr)
     print('--------- END HASH BINARY ---------')
+
+def getLetrasBytes():
+    while True:
+        letras = input("Ingrese un string de máx 32 caracteres: ")
+        if len(letras) <= 32:            
+            # ascii -> 1 byte (8 bits)
+            return letras.encode('ascii')
+        print("Ingrese menos de 32 caracteres!") 
+
+def getProcessesQuantity():
+    while True:
+        cantidadProcesos = input("Ingrese la cantidad de procesos con la que desea calcular: ")
+        try:
+            val = int(cantidadProcesos)
+            if (val < 0 or val > 8):
+                return 1
+            return val
+        except ValueError:
+            print("Ingrese un número SIN caracteres")
+
+#(minValue, maxValue, letras,  )
+#Array ([BoolTick, ValueTick], [])
+def findCollision(minValue, maxValue, letras_bytes, arrayP, valueP):
+    # Tiempos de intervalos internos
+    #internalTick = Ticker()
+    # Intervalo de reporte cada X hashes
+    interval= 348692
+    
+    n = np.uint32(minValue)
+    if n == 0:
+        n = 1
+    exitFlag = False
+    n2 = minValue
+
+    arrayP[1] = False
+    arrayP[4] = False
+    arrayP[3] = 0
+    arrayP[0] = 0
+
+    while (n <= maxValue and exitFlag == False):
+        # Extrae bytes y los guarda en b
+        b = struct.pack('I', n)
+
+        h = hashlib.sha256()
+        h.update(b)
+        digesto = h.digest()
+        exitFlag = try_match_bytes(digesto, letras_bytes)
+        if (exitFlag):
+            arrayP[1] = exitFlag       
+            arrayP[2] = n
+            arrayP[4] = exitFlag        
+
+        
+        if (n % interval == 0):
+            if arrayP[4] == True:
+                exitFlag = True    
+            #di = internalTick()
+            #dn = n - n2
+            arrayP[0] = (n - n2)
+            arrayP[3] = 1
+            n2 = n
+        n+=1
+
+# Imprime el hash encontrado por colisión
+def printCollision(n, letras_bytes):
+    b = struct.pack('I', n)
+
+    h = hashlib.sha256()
+    h.update(b)
+    digesto = h.digest()
+
+    print("Exito!")
+    print("Hash SHA256(", n,") = ", end='')
+    for i in range(len(letras_bytes)):
+        print(chr(digesto[i]), end='')
+    print("...")
+    printHashBinary(digesto, ' ')
--- main.py
+++ main.py
@@ -1,53 +1,73 @@
-from multiprocessing import Pool
-import time
-import numpy as np
-import struct
-import hashlib
+import multiprocessing
+from commonfunctions import Ticker, try_match_bytes, printHashBinary, getLetrasBytes, getProcessesQuantity, findCollision, printCollision
 
-from commonfunctions import Ticker, try_match_bytes, printHashBinary
+if __name__ == '__main__':
+  # String de "letras" a colisionar contra uint32
+  letras_bytes = "UAD"#getLetrasBytes()
+  letras_bytes = letras_bytes.encode('ascii') #borrar
+  # Cantidad de procesos a crear
+  processesQuantity = 2#getProcessesQuantity()
 
-letras = "MA"
-letras_bytes = letras.encode('ascii')
-# Tiempos de intervalos internos
-internalTick = Ticker()
-# Tiempos totales
-tickTotal = Ticker()
+  # Tiempo total de cómputo
+  tickTotal = Ticker()
 
-# Intervalo de reporte cada 
-interval= 348692 # hashes
+  # Máx número entero sin signo de 32 bits. Rango de uint32: 0 - 4294967295
+  maxValue = 4294967295
 
-# Crear un numero entero sin signo de 32 bits.
-# Rango de uint32: 0 - 4294967295
-# hagan la prueba de colocar el valor máximo para ver los bytes
-n = np.uint32(0)
-exitFlag = False
-n2 = 0
-while (n <= 4294967295 and exitFlag == False):
-  # Extraer bytes, los guarda en b
-  b = struct.pack('I', n)
+  # Array de datos de procesos a crear (minValue, maxValue, letras, arrayP, valueP, process )
+  arrayOfProcesses = []
 
-  h = hashlib.sha256()
-  h.update(b)
-  digesto = h.digest()
-  exitFlag = try_match_bytes(digesto, letras_bytes)
-  if (exitFlag):
-    print("EXITO!!!!!!!!")
-    print("Hash SHA256(", n,") = ", end='')
-    for i in range(len(letras_bytes)):
-        print(chr(digesto[i]), end='') 
-    print("...")
-    printHashBinary(digesto, ' ')
-      
-  if (n % interval == 0):
-    di = internalTick()
-    dn = n - n2
-    print("Hashes calculados:", n, " rate: ", round(dn/di, 1), " h/s")  
-    n2 = n
-  n+=1
+  # Ingresa datos para los procesos a crear
+  valorAnterior = 0
+  for i in range(processesQuantity):
+    arrayOfProcesses.append([valorAnterior, round((maxValue/processesQuantity)*(i+1)), letras_bytes, multiprocessing.Array('I', 5), multiprocessing.Value('i', 0)])
+    valorAnterior = round((maxValue/processesQuantity)*(i+1))
 
-# final imprime sumario
-d = tickTotal()
-print("Tiempo requerido para computar ", n, " hashes fue de: ", round(d, 2), " segundos")
-print("Hash rate: ", round(n/d, 1), " hashes/seg")
-print("Tiempo estimado en el cual se alcance el maximo rango del nonce entero de 32 bits")
-print( round((4294967295 / (n/d))/3600, 2), " horas")
+  # Crea los procesos con sus respectivos datos
+  a = arrayOfProcesses
+  for i in range(processesQuantity):
+    arrayOfProcesses[i].append(multiprocessing.Process(target=findCollision, args=(a[i][0], a[i][1], a[i][2], a[i][3], a[i][4])))
+
+  # Inicia los procesos
+  for i in range(processesQuantity):
+    arrayOfProcesses[i][5].start()
+
+  # "Analiza" datos pasados por memoria compartida entre procesos
+  internalTick = Ticker()
+  cantHash = 0
+  cant = 0
+  n2 = 0
+  stopVar = False
+  while stopVar is False:
+    for i in range(processesQuantity):
+      if (arrayOfProcesses[i][3])[3] == 1:        
+        cantHash += (arrayOfProcesses[i][3])[0]
+        (arrayOfProcesses[i][3])[3] = 0
+        cant += 1
+
+      if (arrayOfProcesses[i][3])[1] == True:
+        stopVar = True        
+        printCollision((arrayOfProcesses[i][3])[2],letras_bytes)
+
+    if cant == processesQuantity:
+      di = internalTick()
+      dn = cantHash - n2
+      cant = 0
+      print("HASHES CALCULADOS:", cantHash , " rate: ", round(dn/di, 1), " hash/s")  
+      n2 = cantHash
+
+  for i in range(processesQuantity):
+    (arrayOfProcesses[i][3])[4] = True
+
+  # Tiempo total de trabajo
+  d = tickTotal()
+
+  # Espera que finalicen los procesos creados
+  for i in range(processesQuantity):
+    arrayOfProcesses[i][5].join()
+
+  print("Tiempo requerido para computar ", cantHash, " hashes fue de: ", round(d, 2), " segundos")
+  print("Hash rate: ", round(cantHash/d, 1), " hashes/seg")
+
+  print("Tiempo estimado en el cual se alcance el maximo rango del nonce entero de 32 bits")
+  print( round((4294967295 / (cantHash/d))/3600, 2), " horas")
