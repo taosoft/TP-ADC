5b0a33dc3e198f0a78e9684b5b6c1cc1a9def37d
MATIAS-ASUS <matigrieben@est.frba.utn.edu.ar>
2019-06-28 16:05:10 -0300

    Detalles arreglados -Finalizado

--- commonfunctions.py
+++ commonfunctions.py
@@ -16,14 +16,14 @@ class Ticker:
         return dt
 #-------------------------------------------
 
-#
+# Prueba matchear los bytes del digesto contra las letras a colisionar
 def try_match_bytes (digesto, match_bytes):
     for i in range(len(match_bytes)):
         if (digesto[i] != match_bytes[i]):
             return False
     return True
 
-# 
+# Imprime el Hash en binario
 def printHashBinary(digesto, spacer='', breakOn=4):
     hstr = ""
     pos = 0
@@ -37,6 +37,7 @@ def printHashBinary(digesto, spacer='', breakOn=4):
     print(hstr)
     print('--------- END HASH BINARY ---------')
 
+# Retorna el string ingresado para probar colisión
 def getLetrasBytes():
     while True:
         letras = input("Ingrese un string de máx 32 caracteres: ")
@@ -45,6 +46,7 @@ def getLetrasBytes():
             return letras.encode('ascii')
         print("Ingrese menos de 32 caracteres!") 
 
+# Retorna la cantidad de procesos a crear
 def getProcessesQuantity():
     while True:
         cantidadProcesos = input("Ingrese la cantidad de procesos con la que desea calcular: ")
@@ -56,11 +58,12 @@ def getProcessesQuantity():
         except ValueError:
             print("Ingrese un número SIN caracteres")
 
-#(minValue, maxValue, letras,  )
-#Array ([BoolTick, ValueTick], [])
+# (minValue, maxValue, letras, arrayMemoriaCompartida[6], valorMemoriaCompartida)
+# -arrayMemoriaCompartida (CantHashPorIntervalo, HayColisionConHashLetras, n, IntervaloFinalizado, TerminarProceso, ProcesoTerminado)
+# -valueMemoriaCompartida -> hash/s
 def findCollision(minValue, maxValue, letras_bytes, arrayP, valueP):
     # Tiempos de intervalos internos
-    #internalTick = Ticker()
+    internalTick = Ticker()
     # Intervalo de reporte cada X hashes
     interval= 348692
     
@@ -70,13 +73,14 @@ def findCollision(minValue, maxValue, letras_bytes, arrayP, valueP):
     exitFlag = False
     n2 = minValue
 
-    arrayP[1] = False
-    arrayP[4] = False
-    arrayP[3] = 0
     arrayP[0] = 0
+    arrayP[1] = False
+    arrayP[3] = False
+    arrayP[4] = False
+    arrayP[5] = False
 
     while (n <= maxValue and exitFlag == False):
-        # Extrae bytes y los guarda en b
+        # Extrae bytes de n y los guarda en b
         b = struct.pack('I', n)
 
         h = hashlib.sha256()
@@ -87,27 +91,30 @@ def findCollision(minValue, maxValue, letras_bytes, arrayP, valueP):
             arrayP[1] = exitFlag       
             arrayP[2] = n
             arrayP[4] = exitFlag        
-
         
         if (n % interval == 0):
             if arrayP[4] == True:
                 exitFlag = True    
-            #di = internalTick()
-            #dn = n - n2
+            di = internalTick()            
+            dn = n - n2
+            valueP.value = dn/di
             arrayP[0] = (n - n2)
-            arrayP[3] = 1
+            arrayP[3] = True
             n2 = n
         n+=1
 
+    if exitFlag == False:
+        arrayP[5] = True
+        
 # Imprime el hash encontrado por colisión
-def printCollision(n, letras_bytes):
+def printCollision(n, letras_bytes, numProcess):
     b = struct.pack('I', n)
 
     h = hashlib.sha256()
     h.update(b)
     digesto = h.digest()
 
-    print("Exito!")
+    print("¡Exito! Colisión de letras contra hash en el proceso #", numProcess)
     print("Hash SHA256(", n,") = ", end='')
     for i in range(len(letras_bytes)):
         print(chr(digesto[i]), end='')
--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from commonfunctions import Ticker, try_match_bytes, printHashBinary, getLetrasB
 
 if __name__ == '__main__':
   # String de "letras" a colisionar contra uint32
-  letras_bytes = "UAD"#getLetrasBytes()
-  letras_bytes = letras_bytes.encode('ascii') #borrar
+  letras_bytes = getLetrasBytes()
+
   # Cantidad de procesos a crear
-  processesQuantity = 2#getProcessesQuantity()
+  processesQuantity = getProcessesQuantity()
 
   # Tiempo total de cómputo
   tickTotal = Ticker()
@@ -14,13 +14,13 @@ if __name__ == '__main__':
   # Máx número entero sin signo de 32 bits. Rango de uint32: 0 - 4294967295
   maxValue = 4294967295
 
-  # Array de datos de procesos a crear (minValue, maxValue, letras, arrayP, valueP, process )
+  # Array de datos de procesos a crear (minValue, maxValue, letras, arrayP, valueP, process)
   arrayOfProcesses = []
 
   # Ingresa datos para los procesos a crear
   valorAnterior = 0
   for i in range(processesQuantity):
-    arrayOfProcesses.append([valorAnterior, round((maxValue/processesQuantity)*(i+1)), letras_bytes, multiprocessing.Array('I', 5), multiprocessing.Value('i', 0)])
+    arrayOfProcesses.append([valorAnterior, round((maxValue/processesQuantity)*(i+1)), letras_bytes, multiprocessing.Array('I', 6), multiprocessing.Value('d', 0.0)])
     valorAnterior = round((maxValue/processesQuantity)*(i+1))
 
   # Crea los procesos con sus respectivos datos
@@ -33,29 +33,49 @@ if __name__ == '__main__':
     arrayOfProcesses[i][5].start()
 
   # "Analiza" datos pasados por memoria compartida entre procesos
-  internalTick = Ticker()
   cantHash = 0
-  cant = 0
-  n2 = 0
+  cantTime = 0.0
+  cantProcess = 0
+  cantFinalizados = 0
+  contador = 0
+  contadorInterno = 0
   stopVar = False
-  while stopVar is False:
+  stopVar2 = False
+  while stopVar == False:
     for i in range(processesQuantity):
-      if (arrayOfProcesses[i][3])[3] == 1:        
+      if (arrayOfProcesses[i][3])[3] == True:        
         cantHash += (arrayOfProcesses[i][3])[0]
-        (arrayOfProcesses[i][3])[3] = 0
-        cant += 1
+        cantTime += arrayOfProcesses[i][4].value
+        (arrayOfProcesses[i][3])[3] = False
+        cantProcess += 1
+      
+      if contador == 4000000:
+        if (arrayOfProcesses[i][3])[5] == True:
+          cantFinalizados += 1
+        contadorInterno += 1
+        if contadorInterno == processesQuantity:
+          contador = 0
+      else:        
+        contador += 1
 
       if (arrayOfProcesses[i][3])[1] == True:
-        stopVar = True        
-        printCollision((arrayOfProcesses[i][3])[2],letras_bytes)
+        stopVar2 = True
+        (arrayOfProcesses[i][3])[1] = False
+        printCollision((arrayOfProcesses[i][3])[2], letras_bytes, i + 1)
 
-    if cant == processesQuantity:
-      di = internalTick()
-      dn = cantHash - n2
-      cant = 0
-      print("HASHES CALCULADOS:", cantHash , " rate: ", round(dn/di, 1), " hash/s")  
-      n2 = cantHash
+    if cantFinalizados == processesQuantity:
+      stopVar = True
+      print("No se encontró ningún hash que colisione con la palabra ingresada")
+      print("¡Programa finalizado!")
 
+    if cantProcess == processesQuantity:      
+      print("Hashes calculados entre", processesQuantity,"procesos:", cantHash , "- rate:", round(cantTime, 1), "hash/s")  
+      cantProcess = 0
+      cantTime = 0.0
+      if stopVar2 :
+        stopVar = True
+
+  # Carga el valor de referencia para terminar el proceso (en la memoria compartida)
   for i in range(processesQuantity):
     (arrayOfProcesses[i][3])[4] = True
 
